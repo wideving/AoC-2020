70aac6a161937db2796a6821867233a28ff09d0c
Joacim Wideving <joacim@ibsp.net>
2020-12-04 22:01:09 +0100

    day1.5

--- AoC-Day1.playground/Contents.swift
+++ AoC-Day1.playground/Contents.swift
@@ -6,6 +6,8 @@ let inputs = data.split(separator: "\n").map { Int(String($0))! }
 
 var sum = 0
 
+//Day1
+
 outerLoop: for i in 0..<inputs.count {
     for j in 0..<inputs.count {
         if inputs[i] + inputs[j] == 2020 {
@@ -17,4 +19,19 @@ outerLoop: for i in 0..<inputs.count {
 }
 
 print(sum)
+
+//Day1.5
+
+outerLoop: for i in 0..<inputs.count {
+    for j in 0..<inputs.count {
+        for k in 0..<inputs.count {
+            if inputs[i] + inputs[j] + inputs[k] == 2020 {
+                print("numbers are \(inputs[i]) and \(inputs[j]) and \(inputs[k])")
+                sum = inputs[i] * inputs[j] * inputs[k]
+                break outerLoop
+            }
+        }
+    }
+}
+
 
