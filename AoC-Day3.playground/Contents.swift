255a1c199d716b4823434b7bbe402579c998c218
Joacim Wideving <joacim@ibsp.net>
2020-12-05 22:37:31 +0100

    Day3.5

--- AoC-Day3.playground/Contents.swift
+++ AoC-Day3.playground/Contents.swift
@@ -4,11 +4,27 @@ let url = Bundle.main.url(forResource: "data", withExtension: "txt")!
 let data = try! String(contentsOf: url)
 var inputs = data.split(separator: "\n")
 
+struct Slope {
+    let right: Int
+    let down: Int
+}
+
 var column = 0
+let maxColumns = inputs[0].count
+
 var trees = 0
 
-var maxColumns = inputs[0].count
+var sum = 1
 
+var slopes = [
+    Slope(right: 1, down: 1),
+    Slope(right: 3, down: 1),
+    Slope(right: 5, down: 1),
+    Slope(right: 7, down: 1),
+    Slope(right: 1, down: 2)
+]
+
+//Day3
 inputs.forEach { row in
     let index = row.index(row.startIndex, offsetBy: (column % maxColumns))
     if (row[index] == "#") {
@@ -17,5 +33,20 @@ inputs.forEach { row in
     column += 3
 }
 
-print(trees)
+//Day3.5
+slopes.forEach { slope in
+    column = 0
+    trees = 0
+    for index in stride(from: 0, to: inputs.count, by: slope.down) {
+        let row = inputs[index]
+        let index = row.index(row.startIndex, offsetBy: (column % maxColumns))
+        if (row[index] == "#") {
+            trees += 1
+        }
+        column += slope.right
+    }
+    sum *= trees
+}
+
+print(sum)
 
