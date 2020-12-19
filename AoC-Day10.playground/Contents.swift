2e310fa40fdbe4d0df1038448e3c9b691503ed51
Joacim Wideving <joacim@ibsp.net>
2020-12-19 20:58:05 +0100

    Update

--- AoC-Day10.playground/Contents.swift
+++ AoC-Day10.playground/Contents.swift
@@ -3,14 +3,13 @@ import UIKit
 let url = Bundle.main.url(forResource: "data", withExtension: "txt")!
 let data = try! String(contentsOf: url)
 var inputs = data.components(separatedBy: "\n").compactMap { Int($0) }.sorted()
-print(inputs)
-var differences = [Int: Int]()
 
+var differences = [Int: Int]()
+differences[inputs[0]] = 1
+differences[3] = 1
+
+//Day 10
 for i in 0..<inputs.count - 1 {
-    if i == 0 {
-        differences[inputs[0]] = inputs[0]
-    }
-    
     let diff = inputs[i + 1] - inputs[i]
     if differences[diff] != nil {
         differences[diff]! += 1
@@ -19,13 +18,12 @@ for i in 0..<inputs.count - 1 {
     }
 }
 
-if differences[3] != nil {
-    differences[3]! += 1
-}
-
 if let ones = differences[1], let threes = differences[3] {
     print("Result: \(ones * threes)")
 } else {
     print("Ones or threes are missing")
 }
+
+//Day 10.5
+
 
